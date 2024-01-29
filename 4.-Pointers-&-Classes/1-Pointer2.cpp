8d778307fa6fb875327bb3bd4e2589399c74e449
danialmd81 <danialmobinidh81@gmail.com>
2024-01-30 01:55:46 +0330

    Pointer & Classes

---
+++ 4.-Pointers-&-Classes/1-Pointer2.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+
+    int *num1 = new int(11);
+    int *num2 = num1;
+
+    *num1 = 22;
+
+    cout << "num1 = " << *num1 << endl;
+    cout << "num2 = " << *num2 << endl;
+}
